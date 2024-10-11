29947b9728c1f5e8419f53f4762e63f950a36d50
elmerurbina <[elmerurbina570@gmail.com]>
2024-10-11 17:01:40 -0600

    Chatbot assitant added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 app.include_router(market_analysis.router, prefix="/market_analysis", tags=["market_analysis"])
 app.include_router(pricing.router, prefix="/pricing", tags=["pricing"])
 app.include_router(recommender.router, prefix="/recommender", tags=["recommender"])
-app.include_router(chatbot.router, prefix="/chatbot", tags=["chatbot"])
+#app.include_router(chatbot.router, prefix="/chatbot", tags=["chatbot"])
 
 @app.get("/")
 def read_root():
